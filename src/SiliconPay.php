3853e2176321d93648ec8099b05437b1008b70f3
MikeOmoding <omodingmike@gmail.com>
2022-10-18 15:19:40 +0300

    Initial commit

---
+++ src/SiliconPay.php
@@ -0,0 +1,43 @@
+<?php
+
+    namespace Omodingmike\Siliconpay;
+
+    use GuzzleHttp\Client;
+
+    class SiliconPay
+    {
+        protected string $api_key;
+        protected string $callback_url;
+        protected string $txRef;
+        protected string $payment_url;
+
+        /**
+         * @param string $api_key
+         * @param string $callback_url
+         * @param string $txRef
+         * @param string $payment_url
+         */
+        public function __construct(string $api_key, string $callback_url, string $txRef, string $payment_url)
+        {
+            $this->api_key      = $api_key;
+            $this->callback_url = $callback_url;
+            $this->txRef        = $txRef;
+            $this->payment_url  = $payment_url;
+        }
+
+        public function pay($amount, $phone, $email)
+        : array
+        {
+            $payload = [
+                'req'            => 'mobile_money',
+                'currency'       => 'UGX',
+                'phone'          => $phone,
+                'encryption_key' => $this->api_key,
+                'amount'         => $amount,
+                'emailAddress'   => $email,
+                'call_back'      => $this->callback_url,
+                'txRef'          => $this->txRef
+            ];
+            return Http::dd()->post($this->payment_url, $payload)->json();
+        }
+    }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,95 @@
+<?php return array(
+    'root' => array(
+        'name' => 'omodingmike/siliconpay',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'guzzlehttp/guzzle' => array(
+            'pretty_version' => '7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => '414c24961042f6616fb43e23fa69a785f9fc053e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/guzzle',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'guzzlehttp/promises' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b94b2807d85443f9719887892882d0329d1e2598',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/promises',
+            'aliases' => array(
+                0 => '1.5.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'guzzlehttp/psr7' => array(
+            'pretty_version' => '1.x-dev',
+            'version' => '1.9999999.9999999.9999999-dev',
+            'reference' => 'e98e3e6d4f86621a9b75f623996e6bbdeb4b9318',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/psr7',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'omodingmike/siliconpay' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/http-client' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '22b2ef5687f43679481615605d7a15c557ce85b1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-client',
+            'aliases' => array(
+                0 => '1.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-client-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'psr/http-message' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'efd67d1dc14a7ef4fc4e518e7dee91c271d524e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-message',
+            'aliases' => array(
+                0 => '1.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-message-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'ralouphie/getallheaders' => array(
+            'pretty_version' => '3.0.3',
+            'version' => '3.0.3.0',
+            'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ralouphie/getallheaders',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
